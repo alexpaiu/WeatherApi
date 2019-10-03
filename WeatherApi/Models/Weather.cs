7a78363d61a2a3eeb969471a22fad93a4aafa5fb
alexpaiu <alex.paiu23@gmail.com>
2019-10-03 11:08:44 +0100

    first commit

---
+++ WeatherApi/Models/Weather.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Web.Script.Serialization;
+
+namespace WeatherApi.Models
+{
+    class Weather
+    {
+        public Object getWeatherForcast()
+        {
+            string url = "https://api.openweathermap.org/data/2.5/weather?q=London&APPID=da794fc3bcbfc36af0bc149b3fdff45b&units=metric";
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<Object>(content);
+            return jsonContent;
+        }
+
+
+    }
+}
